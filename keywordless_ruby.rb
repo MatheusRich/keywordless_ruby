e7e3fb9b089b909726ffdee56e6e40e97e5d1153
Matheus Richard <matheusrichardt@gmail.com>
2020-08-19 02:05:50 -0300

    😈

---
+++ keywordless_ruby.rb
@@ -0,0 +1,167 @@
+# require 'bundler/inline'
+
+# gemfile do
+#   source 'https://rubygems.org'
+  # gem 'debug_inspector'
+  # gem 'rubocop', require: false
+# end
+
+# require 'debug_inspector'
+
+$main_obj = self
+
+class Object
+  def self.initializer(&block)
+    define_method(:initialize, &block)
+  end
+  
+  def ivar_set(name, value)
+    instance_variable_set(:"@#{name}", value)
+  end
+
+  def def_singleton_method(name, &block)
+    define_singleton_method(name, &block)
+  end
+
+  def if_true
+    yield
+
+    self
+  end
+
+  def if_false
+    self
+  end
+
+  def plus(b)
+    self + b
+  end
+
+  def minus(b)
+    self - b
+  end
+
+  def multiply(b)
+    self * b
+  end
+
+  def divide(b)
+    self / b
+  end
+
+  def mod(b)
+    self % b
+  end
+
+
+  def gt?(b)
+    self > b
+  end
+
+  def gte?(b)
+    self >= b
+  end
+
+  def lte?(b)
+    self <= b
+  end
+
+  def lt?(b)
+    self < b
+  end
+
+end
+
+class FalseClass
+  def if_true
+    self
+  end
+
+  def if_false
+    yield
+
+    self
+  end
+end
+
+class NilClass
+  def if_true
+    self
+  end
+
+  def if_false
+    yield
+
+    self
+  end
+end
+
+class Symbol
+  def is(value)
+    assign(self, value)
+  end
+end
+
+def defclass(name, inherits: Object, &block)
+  klass = Class.new(inherits, &block)
+
+  Kernel.const_set(name, klass)
+end
+
+def defmodule(name, &block)
+  klass = Module.new(&block)
+
+  Kernel.const_set(name, klass)
+end
+
+def defmethod(name, &block)
+  define_method(name, &block)
+end
+
+def const(name, value)
+  Kernel.const_set(name, value)
+end
+
+def assign(a, b)
+  $main_obj.instance_variable_set("@#{a}", b)
+  $main_obj.class.class_eval("attr_accessor :#{a}")
+
+  b
+end
+
+# ##############################################################################
+
+defclass :User do
+  initializer do |name:|
+    ivar_set(:name, name)
+  end
+
+  def_singleton_method :with do |*args, **kwargs|
+    new(*args, **kwargs)
+  end
+
+  defmethod(:name) { @name }
+end
+
+defmodule :Asdf do
+  def_singleton_method :asdf do
+    puts 'asdf'
+  end
+end
+
+pp User.with(name: 'matheus').name
+
+Asdf.asdf
+
+:a.is 1
+assign(:b, 2)
+const(:C, 3)
+
+puts 'a:'.plus a.to_s
+puts 'b:'.plus b.to_s
+
+a.gt? b
+  .if_true { puts 'a is greater' }
+  .if_false { puts 'b is greater' }
+
+puts C
